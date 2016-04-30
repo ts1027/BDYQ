592bdea46b6af0442d5f339f844e8180b4e65b4c
ts1027 <443633568@qq.com>
2016-05-01 00:59:35 +0800

    这是我的百度舆情

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        direction: 'vertical',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 0,
+        mousewheelControl: true
+    });
+})
